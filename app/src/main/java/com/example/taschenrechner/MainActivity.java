95c44ef9645fdced70497d1aa428be1d28a9f630
wagenknecht <70338036+FWDL@users.noreply.github.com>
2022-04-08 15:05:28 +0200

    Funktionalität

--- app/src/main/java/com/example/taschenrechner/MainActivity.java
+++ app/src/main/java/com/example/taschenrechner/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.taschenrechner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,102 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView calculation = findViewById(R.id.textCalculation);
     }
+
+    public void onClickBtn0(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"0");
+        calculate(view);
+    }
+
+    public void onClickBtn1(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"1");
+        calculate(view);
+    }
+
+    public void onClickBtn2(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"2");
+        calculate(view);
+    }
+
+    public void onClickBtn3(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"3");
+        calculate(view);
+    }
+
+    public void onClickBtn4(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"4");
+        calculate(view);
+    }
+
+    public void onClickBtn5(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"5");
+        calculate(view);
+    }
+
+    public void onClickBtn6(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"6");
+        calculate(view);
+    }
+
+    public void onClickBtn7(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"7");
+        calculate(view);
+    }
+
+    public void onClickBtn8(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"8");
+        calculate(view);
+    }
+
+    public void onClickBtn9(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"9");
+        calculate(view);
+    }
+
+    public void onClickBtnPlus(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"+");
+    }
+
+    public void onClickBtnMinus(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"-");
+    }
+
+    public void onClickBtnMultiply(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"*");
+    }
+
+    public void onClickBtnDivide(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(calculation.getText()+"/");
+    }
+
+    public void onClickBtnSolve(View view) {
+        TextView solution = findViewById(R.id.textSolution);
+        TextView calculation = findViewById(R.id.textCalculation);
+        calculation.setText(solution.getText());
+        solution.setText("");
+    }
+
+    public void calculate(View view) {
+        TextView calculation = findViewById(R.id.textCalculation);
+        Expression e = new ExpressionBuilder(calculation.getText().toString()).build();
+        TextView solution = findViewById(R.id.textSolution);
+        double result = e.evaluate();
+        solution.setText(Double.toString(result));
+    }
+
 }
